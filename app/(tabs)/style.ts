fb308aeb77c0f5b10d36f4b0e9c043f5bf8f04da
Mikeal <mwiemszra@gmail.com>
2024-03-10 22:29:30 +0100

    💄 Add external styles

---
+++ app/(tabs)/style.ts
@@ -0,0 +1,13 @@
+const style = {
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+};
+
+export default style;
---
+++ assets/styles/global.ts
@@ -0,0 +1,9 @@
+const global = {
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+};
+
+export default global;
