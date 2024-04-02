c4661c342041cc720a8e661e15c97fd5dec0b914
Mikeal <mwiemszra@gmail.com>
2024-04-02 20:13:15 +0200

    🔥 Remove external styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,12 @@
-// Styles
-import global from '@/assets/styles/global';
-import styles from './style';
 // Components
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
   return (
-    <View style={global.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    <View>
+      <Text>Tab One</Text>
+      <View lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,15 +1,12 @@
-// Styles
-import global from '@/assets/styles/global';
-import styles from './style';
 // Components
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabTwoScreen() {
   return (
-    <View style={global.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    <View>
+      <Text>Tab Two</Text>
+      <View lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
